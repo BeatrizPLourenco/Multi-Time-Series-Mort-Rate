a8a738ed8ed96d3c543b401e92d79610e03ac4fb
Beatriz <beatriz.plourenco99@gmail.com>
2022-12-28 23:45:14 +0000

    adding fit function + commenting code + simplify

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ Created on Fri Oct 14 18:23:31 2022
 """
 
 from data_cleaning import preprocess_country_data, split_data
-from preprocessing import  preprocessed_data
-from preprocessing_transformer import preprocessed_data, data_to_logmat, preprocessing_with_both_genders
+import preprocessing_transformer as prt
+import train_transformer as trt
 from scheduler import Scheduler
 import mortalityRateTransformer as mrt
 from torch import from_numpy, nn, optim
-from train_transformer import train, evaluate
 import math, time, copy
 
 if __name__ == "__main__":
@@ -30,6 +29,23 @@ if __name__ == "__main__":
     split_value2 = 2000
     gender = 'both'
     both_gender_model = (gender == 'both')
+
+    # Model hyperparameters  
+    input_size = 5
+    batch_first = True
+    batch_size = 5
+    d_model = 512
+    n_decoder_layers = 4 
+    n_encoder_layers = 4
+    n_heads = 8
+    T_encoder = 7
+    T_decoder = 3
+    dropout_encoder = 0.2 
+    dropout_decoder = 0.2
+    dropout_pos_enc = 0.1
+    dim_feedforward_encoder = 2048
+    dim_feedforward_decoder = 2048
+    num_predicted_features = 1
     
     # Preprocessing
     data = preprocess_country_data(country, raw_filenamePT, raw_filenameSW)
@@ -41,48 +57,45 @@ if __name__ == "__main__":
     
     # preprocessing for the transformer
     if gender == 'both':
-        xe, xd, gender_idx, yd = preprocessing_with_both_genders(training_data, (T_encoder, T_decoder), tau0)
+        xe, xd, gender_idx, yd = prt.preprocessing_with_both_genders(training_data, (T_encoder, T_decoder), tau0)
         xe, xd, gender_idx, yd  = from_numpy(xe).float(), from_numpy(xd).float(), from_numpy(gender_idx).float(), from_numpy(yd).float()
 
-        xe_val, xd_val, gender_idx_val, yd_val = preprocessing_with_both_genders( validation_data, (T_encoder, T_decoder), tau0)
+        xe_val, xd_val, gender_idx_val, yd_val = prt.preprocessing_with_both_genders( validation_data, (T_encoder, T_decoder), tau0)
         xe_val, xd_val, gender_idx_val, yd_val  = from_numpy(xe_val).float(), from_numpy(xd_val).float(), from_numpy(gender_idx_val).float(), from_numpy(yd_val).float()
 
-        xe_test, xd_test, gender_idx_test, yd_test = preprocessing_with_both_genders(test_data, (T_encoder, T_decoder), tau0)
+        xe_test, xd_test, gender_idx_test, yd_test = prt.preprocessing_with_both_genders(test_data, (T_encoder, T_decoder), tau0)
         xe_test, xd_test, gender_idx_test, yd_test  = from_numpy(xe_test).float(), from_numpy(xd_test).float(), from_numpy(gender_idx_test).float(), from_numpy(yd_test).float()
 
 
     elif gender == 'Male' or gender == 'Female' :
-        xe, xd, gender_idx, yd = preprocessed_data( training_data, gender, (T_encoder, T_decoder), tau0, model = "transformer")
+        xe, xd, gender_idx, yd = prt.preprocessed_data( training_data, gender, (T_encoder, T_decoder), tau0, model = "transformer")
         xe, xd, gender_idx, yd  = from_numpy(xe).float(), from_numpy(xd).float(), from_numpy(yd).float()
 
-        xe_val, xd_val, gender_idx_val, yd_val= preprocessed_data( validation_data, gender, (T_encoder, T_decoder), tau0, model = "transformer")
+        xe_val, xd_val, gender_idx_val, yd_val= prt.preprocessed_data( validation_data, gender, (T_encoder, T_decoder), tau0, model = "transformer")
         xe_val, xd_val, gender_idx_val, yd_val = from_numpy(xe_val).float(), from_numpy(xd_val).float(), from_numpy(yd_val).float()
 
-        xe_test, xd_test, gender_idx_test, yd_test = preprocessed_data(test_data, gender, (T_encoder, T_decoder), tau0, model = "transformer")
+        xe_test, xd_test, gender_idx_test, yd_test = prt.preprocessed_data(test_data, gender, (T_encoder, T_decoder), tau0, model = "transformer")
         xe_test, xd_test, gender_idx_test, yd_test  = from_numpy(xe_test).float(), from_numpy(xd_test).float(), from_numpy(yd_test).float()
 
     train_data = (xe, xd, gender_idx, yd)
-    eval_data = (xe_val, xd_val, gender_idx_val, yd_val )
+    val_data = (xe_val, xd_val, gender_idx_val, yd_val )
     test_data = (xe_test, xd_test, gender_idx_test, yd_test)
-
-    
-    ## Model parameters
-    d_model = 512 # This can be any value divisible by n_heads. 512 is used in the original transformer paper.
-    n_heads = 8 # The number of attention heads (aka parallel attention layers). dim_val must be divisible by this number
-    n_decoder_layers = 4 # Number of times the decoder layer is stacked in the decoder
-    n_encoder_layers = 4 # Number of times the encoder layer is stacked in the encoder
-    input_size = 5 # The number of input variables. 1 if univariate forecasting.
-    output_sequence_length = 10 # Length of the target sequence, i.e. how many time steps should your forecast cover
-    num_predicted_features = 1
-    batch_size = 5
 
 
     model = mrt.MortalityRateTransformer(
         input_size = input_size,
-        d_model = d_model, 
+        batch_first = batch_first,
+        d_model = d_model,
         n_decoder_layers = n_decoder_layers,
         n_encoder_layers = n_encoder_layers,
         n_heads = n_heads,
+        T_encoder = T_encoder,
+        T_decoder = T_decoder,
+        dropout_encoder = dropout_encoder,
+        dropout_decoder = dropout_decoder,
+        dropout_pos_enc = dropout_pos_enc,
+        dim_feedforward_encoder = dim_feedforward_encoder,
+        dim_feedforward_decoder = dim_feedforward_decoder,
         num_predicted_features = num_predicted_features,
         both_gender_model = both_gender_model
     )
@@ -97,25 +110,28 @@ if __name__ == "__main__":
         dim1 = T_decoder,
         dim2 = T_encoder
     )
-    
+
+
+    """
     # Defining loss function and optimizer
     loss = nn.MSELoss()
-
     lr = 0.05
     opt = optim.SGD(model.parameters(), lr = lr)
     scheduler = Scheduler(opt, dim_embed = d_model, warmup_steps = 2)
 
     #opt = optim.Adam(model.parameters(), betas = (0.9, 0.98), eps = 1e-9)
     #scheduler = optim.lr_scheduler.StepLR(opt, 1.0, gamma=0.95, )
+
     
-    #Losses
+    # Losses
     best_val_loss = float('inf')
     epochs = 5
     best_model = None
 
+    
     for epoch in range(1, epochs + 1):
         epoch_start_time = time.time()
-        train(
+        trt.train(
         model = model, 
         train_data = train_data,
         src_mask = xe_mask,
@@ -125,7 +141,7 @@ if __name__ == "__main__":
         lr = lr,
         criterion = loss)
         eval_data = eval_data
-        val_loss = evaluate( model, eval_data, tgt_mask,  xe_mask, loss)
+        val_loss = trt.evaluate( model, eval_data, tgt_mask,  xe_mask, loss)
         val_ppl = math.exp(val_loss)
         elapsed = time.time() - epoch_start_time
         print('-' * 89)
@@ -138,15 +154,38 @@ if __name__ == "__main__":
             best_model = copy.deepcopy(model)
 
         if scheduler is not None:
-            scheduler.step()
+            scheduler.step() """
 
-test_loss = evaluate(best_model, test_data, tgt_mask,  xe_mask, loss)
-training_loss = evaluate(best_model, train_data, tgt_mask,  xe_mask, loss)
+    # Training hyperparameters
+    criterion = nn.MSELoss()
+    lr = 0.05
+    opt = optim.SGD(model.parameters(), lr = lr)
+    scheduler = Scheduler(opt, dim_embed = d_model, warmup_steps = 2)
+    epochs = 5
 
-print('=' * 89)
-print('| End of training | training loss {:5.2f} | test loss {:5.2f} | test ppl {:8.2f}'.format(
-    training_loss, test_loss, math.exp(test_loss)))
-print('=' * 89)
+    # Training
+    best_model = trt.fit(
+        model = model,
+        batch_size = batch_size,
+        epochs = epochs,
+        train_data = train_data,
+        val_data = val_data,
+        xe_mask = xe_mask,
+        tgt_mask = tgt_mask,
+        opt = opt, 
+        criterion = criterion, 
+        lr = lr,
+        scheduler = scheduler
+     )
+
+
+    test_loss = trt.evaluate(best_model, test_data, tgt_mask,  xe_mask, loss)
+    training_loss = trt.evaluate(best_model, train_data, tgt_mask,  xe_mask, loss)
+
+    print('=' * 89)
+    print('| End of training | training loss {:5.2f} | test loss {:5.2f} | test ppl {:8.2f}'.format(
+        training_loss, test_loss, math.exp(test_loss)))
+    print('=' * 89)
 
 
 
--- train_transformer.py
+++ train_transformer.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 """
 Created on Tue Oct 18 14:48:24 2022
 
@@ -11,17 +10,49 @@ import numpy as np
 from tqdm import tqdm
 from random import shuffle, choice
 from torch import Tensor
-import time
-import math
+import math, time, copy
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-bptt = 5
-def get_batch(source: Tensor, i: int):
+
+def get_batch(source: Tensor, i: int) -> tuple[Tensor, Tensor, Tensor, Tensor]:
+    """Gets specific batch in a set.
+
+    Returns:
+        Tensor with desired batch.
+    """
     
     return source[0][i, :,:,:], source[1][i, :], source[2][i, :], source[3][i, :]
 
-def train(model, train_data, tgt_mask, src_mask, epoch, optimizer,lr, criterion) -> None:
+
+
+def train(
+    model: torch.nn.Module,
+    batch_size: int,
+    epoch: int,
+    train_data: Tensor,
+    tgt_mask: Tensor,
+    xe_mask: Tensor,  
+    optimizer: torch.optim, 
+    lr: float = 0.05,
+    criterion = torch.nn.MSELoss) -> None:
+
+    """Trains the model through 1 epoch.
+
+    Args:
+        model: model to train.
+        batch_size: size of each batch.
+        epoch: epoch number.
+        train_data: data for training.
+        tgt_mask: mask Tensor on the decoder input.
+        xe_mask: mask Tensor on the encoder output.
+        optimizer: optimizer of the training.
+        criterion: loss function to minimize.
+        lr: learning rate value.
+
+    """
+
     model.train()  # turn on train mode
     total_loss = 0.
     log_interval = 200
@@ -33,9 +64,9 @@ def train(model, train_data, tgt_mask, src_mask, epoch, optimizer,lr, criterion)
         x, y_input, gender_index, y_expected  = get_batch(train_data, batch_nb)
         seq_len = x.size(1)
 
-        if seq_len != bptt:  # only on last batch
-            src_mask = src_mask[:seq_len, :seq_len]
-        output = model(x, y_input, gender_index, src_mask, tgt_mask)
+        if seq_len != batch_size:  # only on last batch
+            xe_mask = xe_mask[:seq_len, :seq_len]
+        output = model(x, y_input, gender_index, xe_mask, tgt_mask)
         loss = criterion(output, y_expected)
 
         optimizer.zero_grad()
@@ -48,25 +79,116 @@ def train(model, train_data, tgt_mask, src_mask, epoch, optimizer,lr, criterion)
             ms_per_batch = (time.time() - start_time) * 1000 / log_interval
             cur_loss = total_loss / log_interval
             ppl = math.exp(cur_loss)
-            print(f'| epoch {epoch:3d} | {batch_nb:5d}/{num_batches:5d} batches | '
-                f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
+
+            print(f'| epoch {epoch:3d} | {batch_nb:5d}/{num_batches:5d} batches | lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
                 f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
+
             total_loss = 0
             start_time = time.time()
 
-def evaluate(model, eval_data, tgt_mask,  src_mask, criterion) -> float:
+
+
+def evaluate(
+    model: torch.nn.Module, 
+    batch_size: int,
+    data: Tensor,  
+    tgt_mask: Tensor,   
+    xe_mask: Tensor, 
+    criterion = torch.nn.MSELoss) -> float:
+
+    """Calculates the model loss for the given data.
+
+    Args:
+        model: model to train.
+        data: Tensor with data in the format (xe_test, xd_test, gender_idx_test, yd_test)
+        xe_mask: mask Tensor on the encoder output.
+        tgt_mask: mask Tensor on the decoder input.
+    
+    Returns:
+        float with loss value.
+    
+    """
+
     model.eval()  # turn on evaluation mode
     total_loss = 0.
-    num_batches = eval_data[0].size(0)
-    batch_size = eval_data[0].size(1)
+    num_batches = data[0].size(0)
+    batch_size = data[0].size(1)
     num_val_patterns = num_batches * batch_size
 
     with torch.no_grad():
         for batch_nb in range(num_batches):
-            x_val, y_val_input, gender_idx, y_val_expected = get_batch(eval_data, batch_nb)
+            x_val, y_val_input, gender_idx, y_val_expected = get_batch(data, batch_nb)
             seq_len = x_val.size(1)
             if seq_len != bptt:
-                src_mask = src_mask[:seq_len, :seq_len]
-            output = model(x_val, y_val_input, gender_idx , src_mask, tgt_mask)
+                xe_mask = xe_mask[:seq_len, :seq_len]
+            output = model(x_val, y_val_input, gender_idx , xe_mask, tgt_mask)
             total_loss += seq_len * criterion(output, y_val_expected).item()
+
     return total_loss /  (num_val_patterns - 1)
+
+
+def fit(
+    model: torch.nn.Module, 
+    batch_size: int,
+    epochs: int, 
+    train_data: Tensor, 
+    val_data: Tensor, 
+    xe_mask: Tensor, 
+    tgt_mask: Tensor, 
+    opt: torch.optim, 
+    criterion = torch.nn.MSELoss, 
+    lr: float = 0.05,
+    scheduler: torch.optim.lr_scheduler = None) -> torch.nn.Module:
+
+    """Fits the transformer model to the training data.
+
+    Args:
+        model: model to train.
+        batch_size: size of each batch.
+        epochs: maximum number of epochs to train.
+        train_data: data for training.
+        val_data: data for validation.
+        xe_mask: mask Tensor on the encoder output.
+        tgt_mask: mask Tensor on the decoder input.
+        opt: optimizer of the training.
+        criterion: loss function to minimize.
+        lr: learning rate value.
+        scheduler: function responsable to vary the learning rate.
+
+    Returns:
+        Trained model of the class torch.nn.Module with the best model according to the validation data.
+    
+    """
+    best_val_loss = float('inf')
+
+    for epoch in range(1, epochs + 1):
+        epoch_start_time = time.time()
+
+        train(
+        model = model, 
+        batch_size = batch_size,
+        train_data = train_data,
+        xe_mask = xe_mask,
+        tgt_mask = tgt_mask,
+        epoch = epoch, 
+        optimizer = opt, 
+        lr = lr,
+        criterion = criterion)
+
+        val_loss = evaluate(model,batch_size, val_data, tgt_mask, xe_mask, criterion)
+
+        elapsed = time.time() - epoch_start_time
+
+        print('-' * 89)
+        print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | valid loss {val_loss:5.2f}')
+        print('-' * 89)
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            best_model = copy.deepcopy(model)
+
+        if scheduler is not None:
+            scheduler.step()
+        
+    return best_model
+
